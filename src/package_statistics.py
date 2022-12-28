cc24b62c5c02806897f5f3b8d578e3a86374f256
abiola-adeoye <slmadeoye@gmail.com>
2022-12-28 09:50:02 +0100

    refactor(package_statistics): made execution speed faster
    - code made use of 3 for loops
    
    - 1 to read package file, 2 to validate package name,and 3 to start getting statistics
    
    - 2 and 3 were combined so the action is performed in 1 for loop
    
    - total for loops reduced from 3 to 2

--- src/package_statistics.py
+++ src/package_statistics.py
@@ -10,7 +10,7 @@ from .package_statistics_logging import load_logging
 
 
 class DebianContentsFile:
-    def __init__(self, arch):
+    def __init__(self, arch: str):
         load_dotenv()
         self.logger = load_logging(__name__)    # setup logger for debian packages statistics
 
@@ -24,7 +24,7 @@ class DebianContentsFile:
             mirror_url = os.environ['DEBIAN_MIRROR_URL']
             return mirror_url + self.contents_index_file_name
 
-        mirror_url = "http://ftp.uk.debian.org/debian/dists/stable/main/"       #const mirror url used when no env
+        mirror_url = "http://ftp.uk.debian.org/debian/dists/stable/main/"    #const mirror url used when no env
         return mirror_url + self.contents_index_file_name
 
     def download_arch_contents_index_file(self):
@@ -35,7 +35,6 @@ class DebianContentsFile:
             try:
                 wget.download(content_index_download_url)
                 self.logger.info(f"Contents file for {self.arch} downloaded...")
-
             except Exception as err:
                 self.logger.error(f"Error downloading contents file for {self.arch}, link is"
                                   " incorrect or architecture not available for mirror...")
@@ -46,31 +45,41 @@ class DebianContentsFile:
         os.remove(self.contents_index_file_path)
 
 
-class DebPackageStatistics():
+class DebPackageStatistics(DebianContentsFile):
 
-    def __init__(self, arch: str):
-        #setup logger for debian packages statistics
-        self.logger = load_logging(__name__)
-        self.logger.info(f"Debian package statistics for {self.arch} started...")
+    def __init__(self, arch):
+        super().__init__(arch)
+        self.download_arch_contents_index_file()
 
-    # only method to be called outside class
     def get_debian_package_statistics(self):
-        self.__download_arch_contents_index_file()
-        if os.path.isfile('./' + self.contents_index_file_name):
-            contents_index_file_data = self.__read_contents_index_file()
+        arch_content_index_data = self.read_contents_index_file()
+        package_stats = self.get_package_stats(arch_content_index_data)
+        self.order_package_statistics(package_stats)
+        self.delete_contents_index_file()
 
-            all_valid_package_name = self.__get_package_name(contents_index_file_data)
 
-            all_valid_package_name_statistics= self.__get_package_statistics(all_valid_package_name)
+    def read_contents_index_file(self) -> List[str]:
+        content_index_arch_info = []
 
-            self.__order_package_statistics(all_valid_package_name_statistics)
+        self.logger.info("Started reading compressed index file")
+        contents_index_arch_file = gzip.open(self.contents_index_file_name, 'rt', encoding='utf-8')
+        for file_line in contents_index_arch_file:
+            content_index_info = {}
+            split_line = file_line.split()  # split on space, package will always be last index pos
 
-            self.delete_contents_index_file()
+            # code below cleans the filename and package name info
+            # content_index_info['filename'] = self._concat_filename_with_space(split_line[0:-1])
+            content_index_info['package'] = self._split_packagae_names(split_line[-1])
+
+            content_index_arch_info.extend(content_index_info['package'])
+
+        self.logger.info("Obtained package name...")
+        return content_index_arch_info
 
     # fixes filenames with ' ' back together after they've been split
     # not used because it's unrequired for final solution but works
     @staticmethod
-    def __concat_filename_with_space(split_filename: List[str]) -> str:
+    def _concat_filename_with_space(split_filename: List[str]) -> str:
         filename = split_filename[0]
         if len(split_filename) > 1:      #if true, means filename had spaces in it
             for seperated_filename in split_filename[1::]:
@@ -78,37 +87,18 @@ class DebPackageStatistics():
         return filename
 
     @staticmethod
-    def __split_packagae_names(package_list_name: List[str]) -> List[str]:
+    def _split_packagae_names(package_list_name: List[str]) -> List[str]:
         # if true means multiple packages had same filename associated with them
         if ',' in package_list_name:
             return package_list_name.split(',')    # splits the packages name by ',' into a list
         return [package_list_name]
 
-    def __read_contents_index_file(self) -> List[str]:
-        content_index_arch_info = []
-
-        self.logger.info("Started reading compressed index file")
-        contents_index_arch_file =  gzip.open(self.contents_index_file_name, 'rt',encoding='utf-8')
-        for file_line in contents_index_arch_file:
-            content_index_info = {}
-            split_line = file_line.split()  #split on space, package will always be last index pos
-
-            #code below cleans the filename and package name info
-            #content_index_info['filename'] = self.__concat_filename_with_space(split_line[0:-1])
-            content_index_info['package'] = self.__split_packagae_names(split_line[-1])
-
-            content_index_arch_info.extend(content_index_info['package'])
-
-        self.logger.info("Obtained package name...")
-        return content_index_arch_info
-
     """
     called in func below.
-    from the repo, an instruction was given to ignore any package name that does not conform to the structure,
-    the below function checks for the structure and removes any that does not conform to the structure,
-    the idea is the full package name can be split ito at least two places by '/' anything less and it is invalide
+    from the repo, instruction given to ignore package name that don't conform to the structure,
+    the below function checks for the structure and removes any that does not conform to the structure
     """
-    def __validate_package_name(self, package_name_value:str ) -> str:
+    def _validate_package_name(self, package_name_value:str ) -> str:
         if self.arch == 'source':
             return package_name_value
         package_name = package_name_value.split('/')
@@ -116,28 +106,22 @@ class DebPackageStatistics():
             return ""
         return package_name[1]
 
-    def __get_package_name(self, arch_content_index_package_name: List[str]) -> List[str]:
+    def get_package_stats(self, content_index_package_line: List[str]) -> List[str]:
         self.logger.info("Validating package name...")
 
-        valid_package_name = []
-        for package_name in arch_content_index_package_name:
-            checking = self.__validate_package_name(package_name)   #function is defined above
-            valid_package_name.append(checking)
-        return valid_package_name
-
-    def __get_package_statistics(self, validated_package_name: List[str]) ->Dict[str, int]:
-        self.logger.info("Getting statistics of debian packages")
-
         package_statistics = defaultdict(int)
-        for package in validated_package_name:
-            package_statistics[package] +=1
+
+        for package_name in content_index_package_line:
+            valid_package_name = self._validate_package_name(package_name)   #function is defined above
+            package_statistics[valid_package_name] += 1
         return package_statistics
 
     # function orders and prints pacakge statistics with most files in descending order
-    def __order_package_statistics(self, stats: Dict[str, int]):
+    def order_package_statistics(self, stats: Dict[str, int]):
         count = 0
         chars = 50
         stats_sorted = sorted(stats, key=stats.get, reverse=True)
+
         print("\n\n")
         for stat_key in stats_sorted:
             count += 1
